505efb6f1532f7198c59cd64e042986ff26528a4
JohnFreddyTutistar <johnfre.157@gmail.com>
2022-08-12 12:05:58 -0500

    importando libreria pdfmake

--- src/app/cupon.component.ts
+++ src/app/cupon.component.ts
@@ -1,13 +1,37 @@
-import { Component } from "@angular/core";
-import { ConnectableObservable } from "rxjs";
+import { Component, OnInit } from "@angular/core";
+// import { pdfmake } from 'pdfmake/build/pdfmake';
+// import { pdfFonts } from 'pdfmake/build/vfs_fonts';
+
+// pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
     selector: "app-cupon",
     templateUrl: "./cupon.component.html"
 })
 
-export class AppCupon{
+export class AppCupon implements OnInit{
+
+    constructor(){}
+
+    ngOnInit(){
+        
+    }
+
     generarCupon(){
         console.log("me diste click")
+        const pdfcupon: any = {
+            content: [
+                {
+                    text: 'Hola mundo'
+                }
+            ]
+        }
+        //const pdf = pdfMake.createPdf(pdfcupon).download('Formthis.data.input.invoice' + "-" + 'this.data.input.registerDate' + ".pdf");
+        const pdf = pdfMake.createPdf(pdfcupon);
+        pdf.open();
     }
 }
